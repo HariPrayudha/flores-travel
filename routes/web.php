9d0a66f002bb9302d3094c9de7c3cfe13b64e71e
Hari Prayudha <prayudahari@gmail.com>
2025-12-05 22:39:11 +0700

    feat: update storage route to serve files directly from the public disk

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,18 @@
 
 use App\Http\Controllers\StorageLinkController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
 
 Route::get('/', function () {
     return 'flores travel';
 });
 
-Route::get('/make-storage-link', [StorageLinkController::class, 'link']);
+Route::get('/storage/{path}', function ($path) {
+    $file = Storage::disk('public')->path($path);
+
+    if (!file_exists($file)) {
+        abort(404);
+    }
+
+    return response()->file($file);
+})->where('path', '.*');
